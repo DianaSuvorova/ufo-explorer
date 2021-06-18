968136bcf488f94a8df14db31739aff7e2aef02d
Diana Suvorova <diana.suvorova@uber.com>
2021-06-18 11:50:08 -0700

    feat: get react's github url

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { execSync } from 'child_process';
+import { accessSync } from 'fs';
+
+
+const reactRepoCommand = `npm view react repository.url`;
+
 
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+
+  const url = execSync(reactRepoCommand);
+  res.status(200).json({ url: url.toString() });
 }
