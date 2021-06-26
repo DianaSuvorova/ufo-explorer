65cf5c75753d5de6e07f224d1fb92a7b0d77f61e
Diana Suvorova <diana.suvorova@uber.com>
2021-06-26 16:58:02 -0700

    dev: adding eslint and prettier

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,23 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { execSync } from 'child_process';
-import { fromUrl } from 'hosted-git-info';
+import { execSync } from "child_process";
+import { fromUrl } from "hosted-git-info";
+import fetch from "node-fetch";
 
-
-const reactRepoCommand = `npm view react repository.url`;
-
+const reactRepoCommand = "npm view react repository.url";
 
 export default (req, res) => {
-
   const url = execSync(reactRepoCommand).toString();
   const { user, project } = fromUrl(url);
 
   fetch(`https://api.github.com/repos/${user}/${project}`)
-  .then(response => response.json())
-    .then(data => {
+    .then((response) => response.json())
+    .then((data) => {
       res.status(200).json(data);
     })
     .catch((error) => {
-      res.status(400).json('Error:', error);
-  });
-
-}
+      res.status(400).json("Error:", error);
+    });
+};
