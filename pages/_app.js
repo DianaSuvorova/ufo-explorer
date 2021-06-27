21599c0dce10f1d779a94548d269e029c8c99c9e
DianaSuvorova <diana.suvorova@gmail.com>
2021-06-26 17:18:57 -0700

    dev: lint and prettier (#8)
    * dev: adding eslint and prettier
    
    * dev: test
    
    * moving to npm
    
    * eslintcache
    
    * trigger build

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
+// eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
