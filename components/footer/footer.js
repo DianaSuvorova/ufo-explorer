db8b9481cd7ecde5500836804f1b1650cf93373d
Jenna Toff <shamanlvr@gmail.com>
2021-07-02 17:58:06 -0700

    added header and footer

---
+++ components/footer/footer.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import styles from "./footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <ul className={styles.footer__content}>
+        <li className={styles.footer__item}>&copy; 2021 UFO, Inc.</li>
+        <li className={styles.footer__item}>
+          <Link href="/">Terms</Link>
+        </li>
+        <li className={styles.footer__item}>
+          <Link href="/">Privacy</Link>
+        </li>
+        <li className={styles.footer__item}>
+          <Link href="/">Site Map</Link>
+        </li>
+        <li className={styles.footer__item}>
+          <Link href="/about">What is UFO?</Link>
+        </li>
+      </ul>
+    </footer>
+  );
+}
---
+++ pages/about.js
@@ -0,0 +1,23 @@
+import Header from "../components/header/header";
+import Footer from "../components/footer/footer";
+
+export default function About() {
+  return (
+    <>
+      <Header />
+      <p>Hello</p>
+      <p>More things</p>
+      <p>Hello</p>
+      <p>More things</p>
+      <p>Hello</p>
+      <p>More things</p>
+      <p>Hello</p>
+      <p>More things</p>
+      <p>Hello</p>
+      <p>More things</p>
+      <p>Hello</p>
+      <p>More things</p>
+      <Footer />
+    </>
+  );
+}
