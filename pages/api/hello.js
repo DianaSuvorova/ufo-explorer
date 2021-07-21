02b8a7cf339e97e142f907b96cb3a4c8dec817d5
DianaSuvorova <diana.suvorova@gmail.com>
2021-07-21 09:41:37 -0700

    switching to rest to get data from npm (#12)

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,18 +1,16 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { execSync } from "child_process";
 import { fromUrl } from "hosted-git-info";
 import fetch from "node-fetch";
 
-const reactRepoCommand = "npm view react repository.url";
-
 export default (req, res) => {
-  const url = execSync(reactRepoCommand).toString();
-  const { user, project } = fromUrl(url);
-
-  fetch(`https://api.github.com/repos/${user}/${project}`)
+  fetch("https://registry.npmjs.org/react")
     .then((response) => response.json())
-    .then((data) => {
-      res.status(200).json(data);
+    .then((npmResponse) => {
+      const { user, project } = fromUrl(npmResponse.repository.url);
+      fetch(`https://api.github.com/repos/${user}/${project}`)
+        .then((response) => response.json())
+        .then((gitResponse) => {
+          res.status(200).json(gitResponse);
+        });
     })
     .catch((error) => {
       res.status(400).json("Error:", error);
