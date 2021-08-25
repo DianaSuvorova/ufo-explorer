71686127e9ae7bb9d9b6d1c0a6b35e7a33d37b60
Rebecca Burch <58955802+thinklikeadesigner@users.noreply.github.com>
2021-08-25 12:13:58 -0500

    getAccessToken and [...auth] from docs as examples

---
+++ pages/getAccessToken.js
@@ -0,0 +1,20 @@
+// pages/api/products.js
+import { getAccessToken, withApiAuthRequired } from '@auth0/nextjs-auth0';
+
+// Access external API from an API Route docs 👇 
+// https://github.com/auth0/nextjs-auth0/blob/main/EXAMPLES.md#access-an-external-api-from-an-api-route
+
+export default withApiAuthRequired(async function products(req, res) {
+  // If your Access Token is expired and you have a Refresh Token
+  // `getAccessToken` will fetch you a new one using the `refresh_token` grant
+  const { accessToken } = await getAccessToken(req, res, {
+    scopes: ['read:products']
+  });
+  const response = await fetch('https://api.example.com/products', {
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
+  });
+  const products = await response.json();
+  res.status(200).json(products);
+});
