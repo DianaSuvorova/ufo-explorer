31bde70e5aeed6744475abde2c798b836f3e38d5
Rebecca Burch <58955802+thinklikeadesigner@users.noreply.github.com>
2021-08-27 10:48:03 -0500

    login

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,21 +1,22 @@
 // pages/api/auth/[...auth0].js
-import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
+import { handleAuth } from '@auth0/nextjs-auth0';
 
 // Access external API from an API Route docs 👇
 // https://github.com/auth0/nextjs-auth0/blob/main/EXAMPLES.md#access-an-external-api-from-an-api-route
 
-export default handleAuth({
-  async login(req, res) {
-    try {
-      await handleLogin(req, res, {
-        authorizationParams: {
-          audience: 'https://api.example.com/products', // or AUTH0_AUDIENCE
-          // Add the `offline_access` scope to also get a Refresh Token
-          scope: 'openid profile email read:products' // or AUTH0_SCOPE
-        }
-      });
-    } catch (error) {
-      res.status(error.status || 400).end(error.message);
-    }
-  }
-});
+export default handleAuth();
+// {
+//   async login(req, res) {
+//     try {
+//       await handleLogin(req, res, {
+//         authorizationParams: {
+//           audience: 'https://api.example.com/products', // or AUTH0_AUDIENCE
+//           // Add the `offline_access` scope to also get a Refresh Token
+//           scope: 'openid profile email read:products', // or AUTH0_SCOPE
+//         },
+//       });
+//     } catch (error) {
+//       res.status(error.status || 400).end(error.message);
+//     }
+//   },
+// }
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,8 +1,8 @@
-import { fromUrl } from "hosted-git-info";
-import fetch from "node-fetch";
+import { fromUrl } from 'hosted-git-info';
+import fetch from 'node-fetch';
 
 export default (req, res) => {
-  fetch("https://registry.npmjs.org/react")
+  fetch('https://registry.npmjs.org/react')
     .then((response) => response.json())
     .then((npmResponse) => {
       const { user, project } = fromUrl(npmResponse.repository.url);
@@ -13,6 +13,6 @@ export default (req, res) => {
         });
     })
     .catch((error) => {
-      res.status(400).json("Error:", error);
+      res.status(400).json('Error:', error);
     });
 };
