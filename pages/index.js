994d71e4a6107475c4d32591b4d356de3e68c9ce
Rebecca Burch <58955802+thinklikeadesigner@users.noreply.github.com>
2021-09-19 17:07:12 -0500

    undid prettier

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Header from '../components/header/header';
-import Footer from '../components/footer/footer';
-import Main from '../components/main/main';
+import Header from "../components/header/header";
+import Footer from "../components/footer/footer";
+import Main from "../components/main/main";
 
 export default function Home() {
   return (
