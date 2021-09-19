7107f70cbf0de735b14396613f0a41551a6a5716
Rebecca Burch <58955802+thinklikeadesigner@users.noreply.github.com>
2021-09-19 16:59:22 -0500

    more quote stuff

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
+import "../styles/globals.css";
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React from "react";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
