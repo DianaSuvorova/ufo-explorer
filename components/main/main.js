1d6151f7af2fa3be3c805ca8723e17391a9b6542
Rebecca Burch <58955802+thinklikeadesigner@users.noreply.github.com>
2021-09-19 17:01:27 -0500

    undid prettier

--- components/main/main.js
+++ components/main/main.js
@@ -6,14 +6,21 @@ export default function Main() {
   return (
     <main className={styles.main}>
       <div className={styles.main__content}>
-        <div className={styles.main__title}>Friendly open source opportunities explorer</div>
+      <div className={styles.main__title}>
+          Friendly open source opportunities explorer
+        </div>
         <SearchForm />
         <div className={styles.main__description}>
           Feel the power of the community by giving back.
         </div>
       </div>
       <div className={styles.main__image}>
-        <Image src="/planet-illustration.jpg" alt="Planet Illustration" width={336} height={237} />
+      <Image
+          src="/planet-illustration.jpg"
+          alt="Planet Illustration"
+          width={336}
+          height={237}
+        />
       </div>
     </main>
   );
