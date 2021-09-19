ae3c32974be382e03a1649f2c89babe88ca1a8de
Rebecca Burch <58955802+thinklikeadesigner@users.noreply.github.com>
2021-09-19 17:23:18 -0500

    changed back to original

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,9 @@
-/* eslint-disable react/jsx-props-no-spreading */
-/* eslint-disable react/prop-types */
-import React from 'react';
-import { UserProvider } from '@auth0/nextjs-auth0';
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return (
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
-  );
+// eslint-disable-next-line react/prop-types
+function MyApp({ Component, pageProps }) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
