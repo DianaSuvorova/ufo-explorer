ada846ee49f26fa533c49645fd6d14d9f665fb2c
Rebecca Burch <58955802+thinklikeadesigner@users.noreply.github.com>
2021-09-19 17:12:10 -0500

    undid prettier

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,8 @@
 import "../styles/globals.css";
 
-
 // eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
-  /* eslint-disable react/jsx-props-no-spreading */
+// eslint-disable react/jsx-props-no-spreading
   return <Component {...pageProps} />;
 }
 
