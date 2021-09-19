96d3cf4c7b7b99123e84c1221ec197f9e1ccd002
Rebecca Burch <58955802+thinklikeadesigner@users.noreply.github.com>
2021-09-19 14:10:29 -0500

    deleted profile page

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,5 +1,5 @@
-import Link from "next/link";
-import styles from "./footer.module.css";
+import Link from 'next/link';
+import styles from './footer.module.css';
 
 export default function Footer() {
   return (
--- components/main/main.js
+++ components/main/main.js
@@ -1,26 +1,19 @@
-import Image from "next/image";
-import styles from "./main.module.css";
-import SearchForm from "../searchform/searchform";
+import Image from 'next/image';
+import styles from './main.module.css';
+import SearchForm from '../searchform/searchform';
 
 export default function Main() {
   return (
     <main className={styles.main}>
       <div className={styles.main__content}>
-        <div className={styles.main__title}>
-          Friendly open source opportunities explorer
-        </div>
+        <div className={styles.main__title}>Friendly open source opportunities explorer</div>
         <SearchForm />
         <div className={styles.main__description}>
           Feel the power of the community by giving back.
         </div>
       </div>
       <div className={styles.main__image}>
-        <Image
-          src="/planet-illustration.jpg"
-          alt="Planet Illustration"
-          width={336}
-          height={237}
-        />
+        <Image src="/planet-illustration.jpg" alt="Planet Illustration" width={336} height={237} />
       </div>
     </main>
   );
