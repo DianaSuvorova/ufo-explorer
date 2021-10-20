1e6f68589be9f236cd4770ba1478b577a592cbb6
Rebecca Burch <58955802+thinklikeadesigner@users.noreply.github.com>
2021-10-20 12:44:02 -0500

    added the relevant code, that I forgot

--- components/searchform/searchform.js
+++ components/searchform/searchform.js
@@ -1,14 +1,33 @@
+import React, {useState} from "react"
 import styles from "./searchform.module.css";
 
 export default function SearchForm() {
+const [file, setFile] = useState(null)
+  
+function submitHandler(e){
+  e.preventDefault();
+  // TODO send to backend
+}
+
+  function fileHandler(e) {
+    e.preventDefault();
+    setFile(e.target.value)
+  }
+
+
   return (
     <div className={styles.search}>
-      <input
-        type="file"
-        accept=".json"
-        placeholder="Coming soon: provide your package.json so we find projects you use that need your help "
-        className={styles.search__input}
-      />
+      <form onSubmit={submitHandler}>
+        <input
+          type="file"
+          accept=".json"
+          placeholder="Coming soon: provide your package.json so we find projects you use that need your help "
+          className={styles.search__input}
+          onChange={fileHandler}
+          value={file}
+        />
+        <button type="submit">submit</button>
+      </form>
     </div>
   );
 }
