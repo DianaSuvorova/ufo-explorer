9ba489273413e3825f289909361b4025119d567c
Rebecca Burch <58955802+thinklikeadesigner@users.noreply.github.com>
2021-11-07 13:08:14 -0600

    theme toggle basic set up with example toggle implementation

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import "../styles/globals.css";
+import { ThemeProvider } from "next-themes";
 
 // eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
   // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />;
+  return(<ThemeProvider> 
+    <Component {...pageProps} />
+    </ThemeProvider>);
 }
 
 export default MyApp;
