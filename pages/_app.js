31608dcb8a627b0bc616d92e172835aba97e2238
Rebecca Burch <58955802+thinklikeadesigner@users.noreply.github.com>
2021-11-10 12:29:44 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import { ThemeProvider } from "next-themes";
 // eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
   return(<ThemeProvider> 
-  // eslint-disable-next-line react/jsx-props-no-spreading
+         {// eslint-disable-next-line react/jsx-props-no-spreading}
     <Component {...pageProps} />
     </ThemeProvider>);
 }
