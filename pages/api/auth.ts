1948cba4ee0365f680d457a7a813e124f996d220
Diana Suvorova <diana.suvorova@uber.com>
2022-01-02 15:12:29 -0800

    feat: setting a token on httpOnly cookie

---
+++ pages/api/auth.ts
@@ -0,0 +1,33 @@
+import fetch from "node-fetch";
+import type { NextApiRequest, NextApiResponse } from 'next'
+import Cookies from 'cookies';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { code } = req.query;
+  const queryData = {
+    client_id: process.env.GITHUB_CLIENT_ID,
+    client_secret: process.env.GITHUB_SECRET,
+    code,
+  };
+
+  const searchParams = new URLSearchParams(queryData);
+
+  await fetch(
+    `https://github.com/login/oauth/access_token?${searchParams.toString()}`,
+    {
+      method: "POST",
+    }
+  )
+    .then((response) => response.text())
+    .then((text) => {
+      console.log(text);
+      const sParams = new URLSearchParams(text);
+      const accessToken = sParams.get('access_token');
+      const cookies = new Cookies(req, res)
+      cookies.set('access-token', accessToken, {
+          httpOnly: true,
+          sameSite: 'lax'
+      })
+      res.status(200).json({accessToken});
+    })
+};
