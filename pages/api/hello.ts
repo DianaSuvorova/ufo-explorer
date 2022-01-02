5dc010f465305fba5fffbd1ab1eb14ed9f602132
DianaSuvorova <diana.suvorova@gmail.com>
2022-01-02 15:22:28 -0800

    feat: setting a token on httpOnly cookie (#44)
    * feat: setting a token on httpOnly cookie

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,15 +1,20 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { fromUrl } from "hosted-git-info";
 import fetch from "node-fetch";
+import Cookies from 'cookies';
 
-const fetchPackageDetails = async (npmName: string) => {
+const fetchPackageDetails = async (npmName: string, accessToken: string) => {
   const npmUrl = `https://registry.npmjs.org/${npmName}`;
   let pkgDetail;
   await fetch(npmUrl)
     .then((response) => response.json())
     .then(async (npmResponse) => {
       const { user, project } = fromUrl(npmResponse.repository.url);
-      await fetch(`https://api.github.com/repos/${user}/${project}`)
+      await fetch(`https://api.github.com/repos/${user}/${project}`, {
+        headers: {
+          'Authorization': `token ${accessToken}`
+        }
+      })
         .then((response) => response.json())
         .then((gitResponse) => {
           pkgDetail = gitResponse;
@@ -19,8 +24,11 @@ const fetchPackageDetails = async (npmName: string) => {
 };
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const cookies = new Cookies(req, res);
+  const accessToken = cookies.get('access-token');
+
   const dependencies = Object.keys(req.body.dependencies || {});
-  const fetchAll = Promise.all(dependencies.map(fetchPackageDetails));
+  const fetchAll = Promise.all(dependencies.map(npmName => fetchPackageDetails(npmName, accessToken)));
   fetchAll
     .then((result) => {
       res.status(200).json(result);
